b2878a0995943e900f4175f9b3fcf6c88ef3e1fc
Laura Wadleigh <laura.wadleigh@gmail.com>
2018-12-08 12:27:54 -0600

    Change variable name

--- plot_data.py
+++ plot_data.py
@@ -8,14 +8,14 @@ import datetime as dt
 
 def get_data_csv(file_name):
 	""" read data in from a file """
-	headers = ['date time','knob position','set point temp','lv temp','lr hum','br temp','br hum']
+	headers = ['date time','knob position','set point temp','lr temp','lr hum','br temp','br hum']
 	data_frame = pd.read_csv(file_name, names = headers)
 	return data_frame
 
 def main():
 	data_frame = get_data_csv('/Users/laurawadleigh/Dev/thermostat_data/temp_hum_log.csv')
 	data_frame['date time'] = pd.to_datetime(data_frame['date time'])
-	plt.plot(data_frame.loc[:,'date time'],data_frame.loc[:,'lv temp'])
+	plt.plot(data_frame.loc[:,'date time'],data_frame.loc[:,'lr temp'])
 	plt.show()
 
 if __name__=="__main__":
