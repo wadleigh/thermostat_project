49633de51459515590b38f1a435ed9f28c2ba3ec
Laura Wadleigh <laura.wadleigh@gmail.com>
2019-07-08 15:09:30 -0500

    Fix the header for the summer log file without the set point temperature and knob position.
    Also add commented out lines that can be used to plot of subset of the data.  This was added in the process of debugging.  The log file had a bunch of null characters that it probably add when the power went out and it died.  I should switch to a proper database to avoid this kind of problem.

--- plot_data.py
+++ plot_data.py
@@ -8,16 +8,22 @@ import datetime as dt
 
 def get_data_csv(file_name):
 	""" read data in from a file """
-	headers = ['date time','knob position','set point temp','lr temp','lr hum','br temp','br hum']
+	#headers = ['date time','knob position','set point temp','lr temp','lr hum','br temp','br hum']
+	headers = ['date time','lr temp','lr hum','br temp','br hum']
 	data_frame = pd.read_csv(file_name, names = headers)
 	return data_frame
 
 def main():
 	data_frame = get_data_csv('/Users/laurawadleigh/Dev/thermostat_data/temp_hum_log_summer.csv')
+	#data_frame = get_data_csv('/Users/laurawadleigh/Dev/thermostat_data/temp_hum_log.csv')
 	data_frame['date time'] = pd.to_datetime(data_frame['date time'])
 	#plt.plot(data_frame.loc[:,'date time'],data_frame.loc[:,'br temp'],data_frame.loc[:,'date time'],data_frame.loc[:,'br hum'])
 	#plt.plot(data_frame.loc[:,'date time'],data_frame.loc[:,'lr temp'],data_frame.loc[:,'date time'],data_frame.loc[:,'lr hum'])
 	#plt.plot(data_frame.loc[:,'date time'],data_frame.loc[:,'br hum'],data_frame.loc[:,'date time'],data_frame.loc[:,'lr hum'])
+	
+	#startnum=39959
+	#endnum=39969
+	#plt.plot(data_frame.loc[startnum:endnum,'date time'],data_frame.loc[startnum:endnum,'br temp'],data_frame.loc[startnum:endnum,'date time'],data_frame.loc[startnum:endnum,'lr temp'])
 	plt.plot(data_frame.loc[:,'date time'],data_frame.loc[:,'br temp'],data_frame.loc[:,'date time'],data_frame.loc[:,'lr temp'])
 	#plt.plot(data_frame.loc[100:,'date time'],data_frame.loc[100:,'knob position'])
 	plt.show()
