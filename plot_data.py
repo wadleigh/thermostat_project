c763449781b6dfe601916e7c82799cd1bd5f6518
Laura Wadleigh <laura.wadleigh@gmail.com>
2020-03-30 00:57:50 -0500

    Update to new log file

--- plot_data.py
+++ plot_data.py
@@ -8,13 +8,13 @@ import datetime as dt
 
 def get_data_csv(file_name):
 	""" read data in from a file """
-	#headers = ['date time','knob position','set point temp','lr temp','lr hum','br temp','br hum']
-	headers = ['date time','lr temp','lr hum','br temp','br hum']
+	headers = ['date time','knob position','set point temp','lr temp','lr hum','br temp','br hum']
+	#headers = ['date time','lr temp','lr hum','br temp','br hum']
 	data_frame = pd.read_csv(file_name, names = headers)
 	return data_frame
 
 def main():
-	data_frame = get_data_csv('/Users/laurawadleigh/Dev/thermostat_data/temp_hum_log_summer.csv')
+	data_frame = get_data_csv('/Users/laurawadleigh/Dev/thermostat_data/temp_hum_log_fall.csv')
 	#data_frame = get_data_csv('/Users/laurawadleigh/Dev/thermostat_data/temp_hum_log.csv')
 	data_frame['date time'] = pd.to_datetime(data_frame['date time'])
 	#plt.plot(data_frame.loc[:,'date time'],data_frame.loc[:,'br temp'],data_frame.loc[:,'date time'],data_frame.loc[:,'br hum'])
